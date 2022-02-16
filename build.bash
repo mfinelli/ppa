8287621c1045b5ec06a6dfd76f4151ea1c547216
Mario Finelli <mario@finel.li>
2022-02-16 14:15:31 +0100

    Start building initial release script

---
+++ build.bash
@@ -0,0 +1,28 @@
+#!/bin/bash -ex
+
+if [[ $# -ne 1 ]]; then
+  echo >&2 "usage: $(basename "$0") package"
+  exit 1
+fi
+
+codename="$(grep UBUNTU_CODENAME /etc/os-release | awk -F= '{print $2}')"
+
+mkdir -p build-area
+
+# for "backport" sources we have special handling, we need to download the
+# original sources from ubuntu and place them in the correct place.
+# we have very few backport sources, so we just hardcode them here
+if [[ $1 == ansible ]]; then
+  cd build-area
+  # use the UMD mirror -- archive.ubuntu.com is not available over https
+  wget https://mirror.umd.edu/ubuntu/ubuntu/pool/universe/a/ansible/ansible_2.10.7+merged+base+2.10.8+dfsg.orig.tar.xz
+  cd ..
+fi
+
+cd "$1"
+
+brz builddeb -S
+cd ../build-area
+pbuilder-dist "$codename" build *.dsc
+
+exit 0
