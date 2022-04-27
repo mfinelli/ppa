5335a4b09c9a18076af6cf7acb110c93eec146a4
Mario Finelli <mario@finel.li>
2022-04-27 18:15:19 +0200

    Cleanup zip2tar

--- zip2tar.bash
+++ zip2tar.bash
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
+# extracts a zip archive and repackages it as a tar.gz. adapted from:
 # https://gist.github.com/natemcmaster/5e36e3953c586e4b407a7bb9316b8772
+#
+# usage: ./zip2tar.bash src dest
 
-if [[ $# < 2 ]]; then
-    echo "Usage: [src] [dest]"
-    exit 1
+if [[ $# -ne 2 ]]; then
+  echo >&2 "usage $(basename "$0") src dest"
+  exit 1
 fi
 
 function realpath() {
-    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+  [[ $1 == /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 
-src="$(realpath $1)"
-dest="$(realpath $2)"
+src="$(realpath "$1")"
+dest="$(realpath "$2")"
 
 echo "Converting:"
 echo "  -  $src"
@@ -21,13 +24,15 @@ echo "  => $dest"
 tmp="$(mktemp -d)"
 echo "Using temp dir $tmp"
 function cleanup() {
-    echo "Cleaning up"
-    rm -rf $tmp
-    echo "Done"
+  echo "Cleaning up"
+  rm -rf "$tmp"
+  echo "Done"
 }
 trap cleanup INT TERM EXIT
 set -e
 
-unzip -q $src -d $tmp
-chmod -R +r $tmp
-tar -c -z -f $dest -C $tmp .
+unzip -q "$src" -d "$tmp"
+chmod -R +r "$tmp"
+tar -c -z -f "$dest" -C "$tmp" .
+
+exit 0
